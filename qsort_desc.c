c5f357415c839f687994da67fa472e869b4040b1
pranavkartikeya <122658123+pranavkartikeya@users.noreply.github.com>
2025-03-09 11:28:32 +0530

    Create qsort_desc.c

---
+++ qsort_desc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+   int comp(const void* a ,const void* b){
+       return *(int*)b-*(int*)a ;
+
+   }
+
+int main() {
+
+    int arr[]={1,5,6,2,3,9};
+    int len= sizeof(arr)/sizeof(arr[0]);
+    qsort(arr,len,sizeof(int),comp);
+
+    for(int i=0;i<=len-1;i++){
+       printf("%d",arr[i]);
+
+    }
+}
+
